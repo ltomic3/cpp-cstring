de062801ab2f0d08613507f97ea41a0e16185b2b
Lovre Tomic <tomiclovre05@gmail.com>
2022-12-04 09:38:04 +0100

    Updated

--- Praktikumi/p5.cpp
+++ Praktikumi/p5.cpp
@@ -5,6 +5,19 @@
 using namespace std;
 int main(void)
 {
+    int n;
+    cin >> n;
 
+    int brM(0);
+    int brZ(0);
+    char ime[100 + 1];
+    for(int i = 0; i < n; i++) {
+        cin >> ime;
+        if(ime[strlen(ime)-1] == 'a') brZ++;
+        else brM++;
+    }
+
+    if(brZ < brM) cout << brZ;
+    else cout << brM;
     return 0;
 }
